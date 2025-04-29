af432eb849ccdfc60b3e205c0d90cd3844976374
joaovmvmendes <joao.vmvmendes@gmail.com>
2025-04-29 17:35:51 -0300

    Add .env

--- monitor.py
+++ monitor.py
@@ -1,7 +1,10 @@
+from dotenv import load_dotenv
 import requests
 import json
 import os
 import re
+
+load_dotenv()
 
 BOT_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
